00dfcddb38743406a52466b8f6b134887bcfe7be
boby <aww3992@gmail.com>
2023-10-20 19:08:18 +0700

    project

---
+++ Main.java
@@ -0,0 +1,93 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Variabel untuk menyimpan informasi ruangan
+        String namaRuangan;
+        int kapasitasRuangan;
+        double hargaPerJam = 30000, 
+        boolean pesanLagi = true;
+
+        while (pesanLagi) {
+            System.out.println("=======================================");
+            System.out.println("           Aplikasi Booking Ruangan");
+            System.out.println("=======================================");
+            System.out.println("Mau Ngapain? :");
+            System.out.println("1. Pemesanan lengkap");
+            System.out.println("2. Periksa ketersediaan ruangan");
+            System.out.print("Masukkan nomor tindakan (1/2): ");
+            int tindakan = input.nextInt();
+            input.nextLine(); 
+
+            if (tindakan == 1) {
+    
+                System.out.print("Masukkan nama ruangan: ");
+                namaRuangan = input.nextLine();
+
+                System.out.print("Masukkan kapasitas ruangan: ");
+                kapasitasRuangan = input.nextInt();
+
+                System.out.print("Masukkan jumlah jam yang ingin dipesan: ");
+                double jumlahJam = input.nextDouble();
+
+                System.out.print("Masukkan tanggal pemesanan (dd/mm/yyyy): ");
+                String tglPemesanan = input.next();
+
+                // Mendapatkan bulan dari tglPemesanan
+                int bulan = Integer.parseInt(tglPemesanan.split("/")[1]);
+
+                if (bulan >= 1 && bulan <= 4) {
+                    diskonBulan = 0.05;
+                }
+
+                double totalHarga = hargaPerJam * jumlahJam;
+                double jumlahDiskonBulan = totalHarga * diskonBulan;
+                totalHarga -= jumlahDiskonBulan;
+
+                if (kapasitasRuangan > 100) {
+                    double tambahanBiaya = 0.10; // pajak 10% jika kapasitas lebih dari 100t
+                    double jumlahTambahanBiaya = totalHarga * tambahanBiaya;
+                    totalHarga += jumlahTambahanBiaya;
+                } else if (kapasitasRuangan > 50) {
+                    double tambahanBiaya = 0.05; // pajak 5% jika kapasitas lebih dari 50
+                    double jumlahTambahanBiaya = totalHarga * tambahanBiaya;
+                    totalHarga += jumlahTambahanBiaya;
+                }
+
+                System.out.println("\n=======================================");
+                System.out.println("            Detail Pemesanan");
+                System.out.println("=======================================");
+                System.out.println("Nama Ruangan: " + namaRuangan);
+                System.out.println("Kapasitas Ruangan: " + kapasitasRuangan + " orang");
+                System.out.println("Harga per Jam: $" + hargaPerJam);
+                System.out.println("Jumlah Jam yang Dipesan: " + jumlahJam + " jam");
+                System.out.println("Total Harga: $" + totalHarga);
+                
+            } else if (tindakan == 2) {
+                // Periksa ketersediaan ruangan
+                System.out.print("Masukkan kapasitas ruangan yang ingin diperiksa: ");
+                kapasitasRuangan = input.nextInt();
+                if (kapasitasRuangan > 500) {
+                    System.out.println("Ruangan tidak tersedia.");
+                } else {
+                    System.out.println("Ruangan tersedia.");
+                }
+            } else {
+                System.out.println("Tindakan tidak valid.");
+            }
+
+            System.out.print("\nApakah Anda ingin melakukan tindakan lainnya? (iya/tidak): ");
+            String jawaban = input.next();
+            if (jawaban.equalsIgnoreCase("tidak")) {
+               System.out.println("================= Terimakasih Telah Berkunjung ================= ");
+               pesanLagi = false;
+            }
+            input.nextLine(); 
+        }
+
+       
+        input.close();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Sistem-Booking-Ruangan
+
+Nama Kelompok :
+- Boby Rozak Saputra (05)
+- Ivan Rizal AHmadi (13)
+- Pandu Cahya (23)
+
+19/10/2023
